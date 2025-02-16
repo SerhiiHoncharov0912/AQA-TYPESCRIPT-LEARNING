a160b99c79089a18e2ec4288c92c361872b30b7a
Serhii <serhii.honcharov0912@gmail.com>
2025-02-16 23:02:49 +0200

    Commit for Lesson7

--- lesson7/src/arrow-functions.ts
+++ lesson7/src/arrow-functions.ts
@@ -1,4 +1,4 @@
-const arraySum = (arr: string[] | number[]): string => {
+const arraySum22 = (arr: string[] | number[]): string => {
     let sum = '';
     for (const item of arr) {
         sum += item;
@@ -6,13 +6,13 @@ const arraySum = (arr: string[] | number[]): string => {
     return sum;
 };
 
-const arraySum2 = (arr: number[]) : number => {
+const arraySum23 = (arr: number[]) : number => {
     return arr.reduce((acc, item) => acc + item);
 };
 
-const arr = [1, 2, 3, 4, 5];
-console.log(arraySum(arr));
-console.log(arraySum2(arr));
-const arr2 = ['c', 'e', 'a', 'd', 'b'];
-console.log(arraySum(arr2));
+const arr22 = [1, 2, 3, 4, 5];
+console.log(arraySum22(arr22));
+console.log(arraySum23(arr22));
+const arr23 = ['c', 'e', 'a', 'd', 'b'];
+console.log(arraySum22(arr23));
 
