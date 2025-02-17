78f563d81414ce68323554148a712e75beb54c88
Serhii <serhii.honcharov0912@gmail.com>
2025-02-17 22:55:25 +0200

    Lesson 8

--- lesson8/src/index.ts
+++ lesson8/src/index.ts
@@ -1,55 +1,39 @@
-import { MathOperations } from './mAth-operation';
+import { Button } from "./abstraction";
+import { Calculator } from "./calculator";
+import { MathOperations } from "./math-operation";
+import { Repositories, Repository } from "./response-interface";
+import { ShortDto } from "./short-dto";
 
-export class Calculator {
-    private value1 = 0;
-    private value2 = 0;
-    private isValue1Set = false;
-    private isValue2Set = false;
-    private operationType: MathOperations = 0;
-    private isOperationTypeSet = false;
-
-    public addValue1(value: number): void {
-        this.value1 = value;
-        this.isValue1Set = true;
-    }
-
-    public addValue2(value: number): void {
-        this.value2 = value;
-        this.isValue2Set = true;
-    }
-
-    public setOperationType(type: MathOperations): void {
-        this.operationType = type;
-        this.isOperationTypeSet = true;
-    }
-
-    public calculate(): number {
-        if (!this.isValue1Set || !this.isValue2Set || !this.isOperationTypeSet) {
-            throw new Error('Value1, Value2 or operationType is not set');
-        }
-
-        switch (this.operationType) {
-            case MathOperations.Addition: {
-                return this.value1 + this.value2;
-            }
-            case MathOperations.Subtraction: {
-                return this.value1 - this.value2;
-            }
-            case MathOperations.Division: {
-                if (this.value2 == 0) {
-                    throw new Error('Division by zero is impossible');
-                }
-                return this.value1 / this.value2;
-            }
-            case MathOperations.Multiplication: {
-                return this.value1 * this.value2;
-            }
-        }
-    }
+async function functionForTask4(): Promise<Repositories> {
+    const response = await fetch('https://api.github.com/users/hadley/repos');
+    const json = (await response.json()) as Repositories;
+    return json;
 }
+
+async function functionForTask5(repo: Repository): Promise<ShortDto> {
+    const shortDtos = new ShortDto(repo);
+    return shortDtos;
+}
+
+(async () => {
+    const json = await functionForTask4();
+    console.log(json[0]);
+})();
+
+(async () => {
+    const json = await functionForTask4();
+    const shortDto = await functionForTask5(json[0]);
+    console.log(shortDto);
+})();
+
+
 const calculator = new Calculator();
 calculator.addValue1(10);
 calculator.addValue2(15);
 calculator.setOperationType(MathOperations.Division);
 const result = calculator.calculate();
 console.log(result);
+
+// task6
+const button = new Button();
+button.focusElement().clickElement();
